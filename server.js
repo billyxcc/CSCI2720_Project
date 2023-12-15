34c9890a5dddc7f136943c57907418e356916cb7
billyxcc <75386066+billyxcc@users.noreply.github.com>
2023-12-15 16:15:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ db.once('open', function () {
     UserName: {
       type: String,
       required: [true, "Username is required"],
+      unique:true
     },
     PassWord: {
       type: String,
